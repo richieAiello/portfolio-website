c8425ac72269d930c1fea68dd1612b52b8c619da
Richard Aiello <richaiello17@gmail.com>
2022-07-05 15:21:03 -0400

    JSX for Portfolio.js.

--- src/routes/Portfolio.js
+++ src/routes/Portfolio.js
@@ -5,17 +5,70 @@ import Context from "../components/shared/Context"
 export default function Portfolio(props) {
   return (
     <main className="main main--portfolio container">
-      <Project mod="">
+      <Project>
         <ProjectImage title="designo"/>
         <Context
           mod="project"
-          secondary={true}
           heading="Designo"
           text={`
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           `}
           link={true}
           path="https://designo-website-richieaiello.netlify.app/"
+          linkText="Visit Website"
+        />
+      </Project>
+      <Project>
+        <ProjectImage title="photosnap"/>
+        <Context
+          mod="project"
+          secondary={true}
+          heading="Photosnap"
+          text={`
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          `}
+          link={true}
+          path="https://photosnap-website-richieaiello.netlify.app/"
+          linkText="Visit Website"
+        />
+      </Project>
+      <Project>
+        <ProjectImage title="scoot"/>
+        <Context
+          mod="project"
+          heading="Scoot"
+          text={`
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          `}
+          link={true}
+          path="https://scoot-website-richaiello.netlify.app/"
+          linkText="Visit Website"
+        />
+      </Project>
+      <Project>
+        <ProjectImage title="dine"/>
+        <Context
+          mod="project"
+          secondary={true}
+          heading="Dine Restaurant"
+          text={`
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          `}
+          link={true}
+          path="https://dine-restaurant-website-richieaiello.netlify.app/"
+          linkText="Visit Website"
+        />
+      </Project>
+      <Project>
+        <ProjectImage title="creative-agency"/>
+        <Context
+          mod="project"
+          heading="Creative Agency"
+          text={`
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          `}
+          link={true}
+          path="https://creative-agency-website-richieaiello.netlify.app/"
           linkText="Visit Website"
         />
       </Project>

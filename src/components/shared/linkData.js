e0208bbec470388edfc48ecc2d0a1c1561b23fe2
Richard Aiello <richaiello17@gmail.com>
2022-07-08 09:09:51 -0400

    Refactored JSX for svg profile icons. Updated Footer.js.

---
+++ src/components/shared/linkData.js
@@ -0,0 +1,29 @@
+import GitHub from "jsx:../../assets/icons/github.svg";
+import Twitter from "jsx:../../assets/icons/twitter.svg";
+import LinkedIn from "jsx:../../assets/icons/linkedin.svg";
+import FrontEndMentor from "jsx:../../assets/icons/frontendmentor.svg";
+
+const linkData = [
+  {
+    name: "github",
+    url: "https://github.com/richieAiello",
+    svg: <GitHub className="icon" />
+  },
+  {
+    name: "twitter",
+    url: "https://twitter.com/richieAiello",
+    svg: <Twitter className="icon" />
+  },
+  {
+    name: "linkedin",
+    url: "#",
+    svg: <LinkedIn className="icon" />
+  },
+  {
+    name: "frontendmentor",
+    url: "https://www.frontendmentor.io/profile/richieAiello",
+    svg: <FrontEndMentor className="icon" />
+  },
+];
+
+export default linkData;

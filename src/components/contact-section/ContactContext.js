c5ed0a4a9fad7b8be84032017f11d7192650db1a
Richard Aiello <richaiello17@gmail.com>
2022-07-08 11:56:22 -0400

    JSX for ContactContext.js.

--- src/components/contact-section/ContactContext.js
+++ src/components/contact-section/ContactContext.js
@@ -1,5 +1,6 @@
 import Context from '../shared/Context';
 import IconLink from '../shared/IconLink';
+import email from '../../assets/icons/email.svg';
 
 const ContactContext = props => {
   return (
@@ -7,7 +8,14 @@ const ContactContext = props => {
       mod="contact"
       heading="Get In Touch"
       text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      link
+      path="mailto: richard.a.aiello@gmail.com"
+      linkText="Email Me"
     >
+      <address className="contact__email">
+        <img src={email} alt="" className="image" /> :
+        richard.a.aiello@gmail.com
+      </address>
       <div className="contact__icons">
         <IconLink mod="contact" name="github" />
         <IconLink mod="contact" name="twitter" />
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,11 +1,9 @@
 import ContactContext from '../components/contact-section/ContactContext';
-import ContactEmail from '../components/contact-section/ContactEmail';
 
 export default function Contact(props) {
   return (
     <main className="main main--contact container">
       <ContactContext />
-      <ContactEmail />
     </main>
   );
 }

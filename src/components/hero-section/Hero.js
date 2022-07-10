1a54b0a610d42b438cfac3fd61a6299889bd0bb8
Richard Aiello <richaiello17@gmail.com>
2022-07-10 09:51:06 -0400

    Styles for Hero.js on tablet.

--- src/components/hero-section/Hero.js
+++ src/components/hero-section/Hero.js
@@ -1,16 +1,20 @@
-import HeroImage from "./HeroImage";
-import HeroWrapper from "./HeroWrapper";
-import HeroHeading from "./HeroHeading";
-import HeroBtn from "./HeroBtn";
+import HeroImage from './HeroImage';
+import HeroWrapper from './HeroWrapper';
+import HeroHeading from './HeroHeading';
+import HeroBtn from './HeroBtn';
 
 export default function Hero(props) {
   return (
-    <section className="hero container">
-      <HeroImage />
-      <HeroWrapper>
-        <HeroHeading>Richard Aiello Front-End Web Developer</HeroHeading>
-        <HeroBtn onClick={props.onClick} />
-      </HeroWrapper>
+    <section className="hero">
+      <div className="container">
+        <HeroImage />
+        <HeroWrapper>
+          <HeroHeading>
+            Richard Aiello Front-End Web Developer
+          </HeroHeading>
+          <HeroBtn onClick={props.onClick} />
+        </HeroWrapper>
+      </div>
     </section>
   );
 }

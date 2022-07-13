f0bae7088c1db35b79901666dbfb574d1a72c163
Richard Aiello <richaiello17@gmail.com>
2022-07-13 09:17:24 -0400

    Updated text and styles for About.js. Added more accessible focus styles for buttons.

--- src/components/about-section/About.js
+++ src/components/about-section/About.js
@@ -9,13 +9,19 @@ const About = forwardRef((props, ref) => {
       <Context
         mod="about"
         heading="About Me"
-        text={`I’m a junior front-end developer looking for a new role in an exciting company. 
-          I focus on writing accessible HTML, using modern CSS practices and writing clean 
-          JavaScript. When writing JavaScript code, I mostly use React, but I can adapt to 
-          whatever tools are required. I’m based in London, UK, but I’m happy working remotely
-          and have experience in remote teams. When I’m not coding, you’ll find me outdoors.
-          I love being out in nature whether that’s going for a walk, run or cycling. I’d love
-          you to check out my work.`}
+        // Rock climbing has been a passion I have been fortunate enough to come by. The sport is consistently difficult,
+        // and has taught me just how much I am able to accomplish if I allow myself to learn from my failures.
+        text={`I am a junior front-end developer looking for a new role in an exciting company. 
+          My front-end tool of choice has been React, using hooks with functional components.
+          I focus on writing accessible JSX, and implementing modern ES6 JavaScript practices. 
+          When styling my websites I have been utilizing SCSS, with an understanding of modern CSS
+          practices ranging from CSS functions, variables, flex-box, CSS-Grid, keyframes, and more
+          in order to make my websites as responsive as possible, while keeping cross-browser compatibility
+          in mind. I am currently learning Tailwind CSS, with plans to learn Typescript, Redux, and Vite, 
+          eventually moving to Next.js to learn back-end development as well. I currently live in Long Island, 
+          New York, and I am searching for a remote position, preferably full-time. When I am not coding or 
+          reading fantasy novels, you’ll find  me bouldering outdoors. I would love for you to check out
+          my work!`}
         route={true}
         path="/portfolio"
         linkText="View Portfolio"
